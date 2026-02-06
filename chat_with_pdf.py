444144d585d9cca5deefd62b837ef2ae5e0910c4
navaneethchowdary38-lang <navaneethchowdary38@gmail.com>
2026-02-06 19:16:56 +0530

    Update chat_with_pdf.py

--- chat_with_pdf.py
+++ chat_with_pdf.py
@@ -64,7 +64,7 @@ def showman():
 
     if user_question:
         # 🔥 Direct Gemini (NO LangChain, NO v1beta)
-       model = genai.GenerativeModel("gemini-1.0-pro")
+        model = genai.GenerativeModel("gemini-1.0-pro")
 
         # 🔎 Retrieve relevant chunks
         docs = st.session_state["docsearch"].similarity_search(
@@ -119,4 +119,3 @@ def show():
 
     if "docsearch" in st.session_state:
         showman()
-
